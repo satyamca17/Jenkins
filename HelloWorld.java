49209a2479609aa7fe1c7e34a1e04dc3df8f4850
Prasanjit Singh <mailprasanjit@gmail.com>
2017-05-12 14:57:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -19,7 +19,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+        // Prints "Hello World, DevOps has arrived!" to the terminal window.
         System.out.println("Hello, World");
     }
 
